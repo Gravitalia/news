94de11521986dc809e5a17942c6d11530cea50b5
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-06-24 21:07:22 +0200

    Init model development

---
+++ qa/model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import tensorflow_hub as tfhub
+import tensorflow_text as text
+from official.nlp import optimization
+
+import matplotlib.pyplot as plt
+
+tf.get_logger().setLevel("ERROR")

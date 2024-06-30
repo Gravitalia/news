21939df9bcc9eb6df42163a1b3bf87f12c23bd07
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-06-30 16:44:16 +0200

    Add tracing

--- graphql/api/src/main.rs
+++ graphql/api/src/main.rs
@@ -1,17 +1,27 @@
+#![forbid(unsafe_code)]
+#![deny(dead_code, unused_imports, unused_mut, missing_docs)]
+//! GraphQL API.
+
+mod media;
 mod models;
 mod news;
-mod media;
 
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 use warp::Filter;
 
-pub const EPOCH: i32 = 1704067200;
+const DEFAULT_PORT: u16 = 5400;
 
 #[tokio::main]
 async fn main() {
-    let context = warp::any().map(move || news::Context {
-    });
+    let fmt_layer = fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true);
+
+    tracing_subscriber::registry().with(fmt_layer).init();
 
     // Create a filter for the main GraphQL endpoint.
+    let context = warp::any().map(move || news::Context {});
     let graphql_filter = juniper_warp::make_graphql_filter(news::schema(), context);
 
     warp::serve(
@@ -20,8 +30,20 @@ async fn main() {
             .map(|| "OK")
             .or(warp::post()
                 .and(warp::path("graphql").and(graphql_filter))
-                .with(warp::log("warp_server"))),
+                .with(warp::log("warp_server")))
+            .or(warp::get()
+                .and(warp::path("graphiql"))
+                .and(juniper_warp::graphiql_filter(
+                    "/graphql",
+                    Some("/subscriptions"),
+                ))),
     )
-    .run(([127, 0, 0, 1], 80))
+    .run((
+        [127, 0, 0, 1],
+        std::env::var("PORT")
+            .unwrap_or_else(|_| DEFAULT_PORT.to_string())
+            .parse()
+            .unwrap(),
+    ))
     .await
 }

14e10a6d712cdc7abd1a011a8f0d531cf6d95601
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-07-03 22:55:04 +0200

    Add Badge component

---
+++ front/components/Badge.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+/* eslint vue/multi-word-component-names: "off", vue/valid-attribute-name: "off" */
+</script>
+
+<template>
+  <span
+    class="px-2.5 py-0.5 font-medium bg-zinc-50 text-zinc-800 dark:bg-zinc-800 dark:text-zinc-300 border rounded-lg"
+  >
+    <slot />
+  </span>
+</template>

e922b35014323bffa6f2b87cc737dfb53bf21b39
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-07-23 17:38:32 +0200

    Add GraphQL schema

---
+++ graphql/api/src/schema/mod.rs
@@ -0,0 +1,45 @@
+mod mcq;
+mod news;
+
+use juniper::{EmptyMutation, EmptySubscription, RootNode};
+use mcq::QuestionQuery;
+use news::NewsQuery;
+
+/// Define the context for the GraphQL schema.
+#[derive(Clone)]
+pub struct Context {}
+impl juniper::Context for Context {}
+
+/// Define the root query object.
+#[derive(Clone, Copy, Debug)]
+pub struct Query;
+
+#[juniper::graphql_object(context = Context)]
+impl Query {
+    /// Everything related to raw news.
+    fn news() -> NewsQuery {
+        NewsQuery
+    }
+
+    /// Everything related multiple-choice question.
+    fn question() -> QuestionQuery {
+        QuestionQuery
+    }
+}
+
+/// Define the schema using RootNode.
+type Schema = RootNode<
+    'static,
+    Query,
+    EmptyMutation<Context>,
+    EmptySubscription<Context>,
+>;
+
+/// Create the schema instance.
+pub fn schema() -> Schema {
+    Schema::new(
+        Query,
+        EmptyMutation::<Context>::new(),
+        EmptySubscription::<Context>::new(),
+    )
+}

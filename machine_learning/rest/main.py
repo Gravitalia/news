bf602aeb192ef8656e6cc084bdb32bb2cd666204
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-07-25 14:42:02 +0200

    Prepare routes

--- machine_learning/rest/main.py
+++ machine_learning/rest/main.py
@@ -1,16 +1,45 @@
-from typing import Union
 import logging
+import tensorflow as tf
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+#summary_model = tf.keras.models.load_model("")
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
+# Handle prefetch request.
+@app.options("/")
 def read_root():
-    return {"Hello": "World"}
+    return "OK"
 
+# Summary model HTTP API.
+@app.get("/summary/", status_code=200)
+def read_sum(text: str, response: Response):
+    response.status_code = status.HTTP_201_CREATED
+    return "SUM"
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+"""
+Question & answers model HTTP API.
+Generates one question and three answers (one true, two false) based on a text.
+"""
+@app.get("/qa/", status_code=200)
+def read_sum(text: str, response: Response):
+    response.status_code = status.HTTP_201_CREATED
+    return JSONResponse(
+        status_code=200,
+        content={
+            "question": "...",
+            "choices": [],
+            "answer": 0,
+        }
+    )

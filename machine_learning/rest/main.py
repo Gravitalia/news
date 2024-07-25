50b462f1f7d7485abe31b6eb61bcd1c7f34bdc0f
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-07-25 18:17:49 +0200

    Add model

--- machine_learning/rest/main.py
+++ machine_learning/rest/main.py
@@ -1,11 +1,11 @@
 import logging
-import tensorflow as tf
+import unidecode
 
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-#summary_model = tf.keras.models.load_model("")
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 app = FastAPI()
 
@@ -17,6 +17,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+tokenizer = AutoTokenizer.from_pretrained("google/pegasus-large")
+model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-large")
+
 # Handle prefetch request.
 @app.options("/")
 def read_root():
@@ -25,8 +28,10 @@ def read_root():
 # Summary model HTTP API.
 @app.get("/summary/", status_code=200)
 def read_sum(text: str, response: Response):
-    response.status_code = status.HTTP_201_CREATED
-    return "SUM"
+    text = unidecode.unidecode(text)
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding="longest")
+    summary_ids = model.generate(inputs["input_ids"], max_length=2000, min_length=200, length_penalty=0.0, num_beams=4, early_stopping=True)
+    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
 """
 Question & answers model HTTP API.

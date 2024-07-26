33f96bbb018f3b2aa915f12293376158b7a01da4
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-07-26 21:29:34 +0200

    Lint code

--- machine_learning/rest/main.py
+++ machine_learning/rest/main.py
@@ -20,25 +20,37 @@ app.add_middleware(
 tokenizer = AutoTokenizer.from_pretrained("google/pegasus-large")
 model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-large")
 
+
 # Handle prefetch request.
 @app.options("/")
 def read_root():
     return "OK"
 
+
 # Summary model HTTP API.
 @app.get("/summary/", status_code=200)
 def read_sum(text: str, response: Response):
     text = unidecode.unidecode(text)
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding="longest")
-    summary_ids = model.generate(inputs["input_ids"], max_length=2000, min_length=200, length_penalty=0.0, num_beams=4, early_stopping=True)
+    inputs = tokenizer(
+        text, return_tensors="pt", truncation=True, padding="longest"
+    )
+    summary_ids = model.generate(
+        inputs["input_ids"],
+        max_length=2000,
+        min_length=200,
+        length_penalty=0.0,
+        num_beams=4,
+        early_stopping=True,
+    )
     return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
-"""
-Question & answers model HTTP API.
-Generates one question and three answers (one true, two false) based on a text.
-"""
+
 @app.get("/qa/", status_code=200)
 def read_sum(text: str, response: Response):
+    """
+    Question & answers model HTTP API.
+    Generates one question and three answers (one true, two false) based on a text.
+    """
     response.status_code = status.HTTP_201_CREATED
     return JSONResponse(
         status_code=200,
@@ -46,5 +58,5 @@ def read_sum(text: str, response: Response):
             "question": "...",
             "choices": [],
             "answer": 0,
-        }
+        },
     )

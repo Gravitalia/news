0b0c99786f6dd15d06414933697a59f4df9e3d52
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-09 19:56:58 +0200

    Add image extractor

--- graphql/api/src/media/fr.rs
+++ graphql/api/src/media/fr.rs
@@ -1,13 +1,12 @@
 //! France (FR) medias.
 
-use crawler::scraper::Extractor;
+use crawler::scraper::{Attribute, Extract};
 use strum_macros::EnumIter;
 
 #[derive(Debug, EnumIter)]
 pub enum French {
     LHumanité,
     Libération,
-    LaCroix,
     LeMonde,
     LeParisien,
     LesEchos,
@@ -23,7 +22,6 @@ impl French {
         match self {
             French::LHumanité => "L'Humanité",
             French::Libération => "Libération",
-            French::LaCroix => "La Croix",
             French::LeMonde => "Le Monde",
             French::LeParisien => "Le Parisien",
             French::LesEchos => "Les Echos",
@@ -39,7 +37,6 @@ impl French {
         match self {
             French::LHumanité => "https://www.humanite.fr",
             French::Libération => "https://www.liberation.fr",
-            French::LaCroix => "https://www.la-croix.com",
             French::LeMonde => "https://www.lemonde.fr",
             French::LeParisien => "https://www.leparisien.fr",
             French::LesEchos => "https://www.lesechos.fr",
@@ -57,7 +54,6 @@ impl French {
             French::Libération => {
                 Some("https://www.liberation.fr/arc/outboundfeeds/rss-all/collection/accueil-une/")
             }
-            French::LaCroix => Some("https://www.la-croix.com/RSS/UNIVERS"),
             French::LeMonde => Some("https://www.lemonde.fr/rss/une.xml"),
             French::LeParisien => Some("https://feeds.leparisien.fr/leparisien/rss"),
             French::LesEchos => Some("https://services.lesechos.fr/rss/les-echos-monde.xml"),
@@ -69,51 +65,107 @@ impl French {
         }
     }
 
-    pub fn extractor(&self) -> Extractor {
+    pub fn extractor(&self) -> Extract {
         match self {
-            French::LHumanité => Extractor {
-                class: Some("rich-text".to_owned()),
-                id: None,
+            French::LHumanité => Extract {
+                content: Attribute {
+                    class: Some("rich-text".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("wp-image-".to_owned()),
+                    id: None,
+                },
             },
-            French::Libération => Extractor {
-                class: Some("article-body-wrapper".to_owned()),
-                id: None,
+            French::Libération => Extract {
+                content: Attribute {
+                    class: Some("article-body-wrapper".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("sc-".to_owned()),
+                    id: None,
+                },
             },
-            French::LaCroix => Extractor {
-                class: Some("content".to_owned()), // `article-content-wrapper` should also work.
-                id: None,
+            French::LeMonde => Extract {
+                content: Attribute {
+                    class: Some("article__content".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("lzld--loading".to_owned()),
+                    id: None,
+                },
             },
-            French::LeMonde => Extractor {
-                class: Some("article__content".to_owned()),
-                id: None,
+            French::LeParisien => Extract {
+                content: Attribute {
+                    class: Some("article-section".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("image".to_owned()),
+                    id: None,
+                },
             },
-            French::LeParisien => Extractor {
-                class: Some("article-section".to_owned()),
-                id: None,
+            French::LesEchos => Extract {
+                content: Attribute {
+                    class: Some("post-paywall".to_owned()), // `post-paywall` seems to be the unchanged class; however we have `kbgxbh`.
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("sc-".to_owned()),
+                    id: None,
+                },
             },
-            French::LesEchos => Extractor {
-                class: Some("post-paywall".to_owned()), // `post-paywall` seems to be the unchanged class; however we have `kbgxbh`.
-                id: None,
+            French::OuestFrance => Extract {
+                content: Attribute {
+                    class: None, // `contenu-principal` may work.
+                    id: Some("article-detail".to_owned()),
+                },
+                image: Attribute {
+                    class: Some("su-media".to_owned()),
+                    id: None,
+                },
             },
-            French::OuestFrance => Extractor {
-                class: None, // `contenu-principal` may work.
-                id: Some("article-detail".to_owned()),
+            French::LePoint => Extract {
+                content: Attribute {
+                    class: None, // `article-styles` may work.
+                    id: Some("contenu".to_owned()),
+                },
+                image: Attribute {
+                    class: None,
+                    id: None,
+                },
             },
-            French::LePoint => Extractor {
-                class: None, // `article-styles` may work.
-                id: Some("contenu".to_owned()),
+            French::LExpress => Extract {
+                content: Attribute {
+                    class: Some("qiota_reserve".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("illustration__image".to_owned()), // `img_resp_full` should also work.
+                    id: None,
+                },
             },
-            French::LExpress => Extractor {
-                class: Some("qiota_reserve".to_owned()),
-                id: None,
+            French::Marianne => Extract {
+                content: Attribute {
+                    class: Some("article__content".to_owned()), // `article__wrapper` should also work.
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("responsive-image".to_owned()),
+                    id: None,
+                },
             },
-            French::Marianne => Extractor {
-                class: Some("article__content".to_owned()), // `article__wrapper` should also work.
-                id: None,
-            },
-            French::ValeursActuelles => Extractor {
-                class: Some("post__content".to_owned()),
-                id: None,
+            French::ValeursActuelles => Extract {
+                content: Attribute {
+                    class: Some("post__content".to_owned()),
+                    id: None,
+                },
+                image: Attribute {
+                    class: Some("attachment-post-thumbnail".to_owned()),
+                    id: None,
+                },
             },
         }
     }

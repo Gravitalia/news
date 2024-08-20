0bf024b8000f2ffc29d27ed2aca55e084c42aab2
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-20 21:31:57 +0200

    Use multilingual model

--- machine_learning/rest/main.py
+++ machine_learning/rest/main.py
@@ -1,5 +1,6 @@
 import logging
 import unidecode
+import re
 
 from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
@@ -17,8 +18,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-tokenizer = AutoTokenizer.from_pretrained("google/pegasus-xsum")
-model = AutoModelForSeq2SeqLM.from_pretrained("google/pegasus-xsum")
+tokenizer = AutoTokenizer.from_pretrained("csebuetnlp/mT5_multilingual_XLSum")
+model = AutoModelForSeq2SeqLM.from_pretrained("csebuetnlp/mT5_multilingual_XLSum")
 
 
 # Handle prefetch request.
@@ -27,22 +28,40 @@ def read_root():
     return "OK"
 
 
-# Summary model HTTP API.
+WHITESPACE_HANDLER = lambda k: re.sub('\s+', ' ', re.sub('\n+', ' ', k.strip()))
+
 @app.get("/summary/", status_code=200)
 def read_sum(text: str, response: Response):
+    """
+    Summary model HTTP API.
+    """
     text = unidecode.unidecode(text)
-    inputs = tokenizer(
-        text, return_tensors="pt", truncation=True, padding="longest"
-    )
-    summary_ids = model.generate(
-        inputs["input_ids"],
-        max_length=2000,
-        min_length=200,
+
+    input_ids = tokenizer(
+        [WHITESPACE_HANDLER(text)],
+        return_tensors="pt",
+        padding="max_length",
+        truncation=True,
+        max_length=4096
+    )["input_ids"]
+    output_ids = model.generate(
+        input_ids=input_ids,
+        min_length=100,
+        max_length=1500,
+        no_repeat_ngram_size=4,
+        num_beams=6,
+        temperature=0.7,
+        top_k=20,
         length_penalty=0.0,
-        num_beams=4,
-        early_stopping=True,
+        repetition_penalty=2.0,
+        do_sample=True,
+    )[0]
+
+    return tokenizer.decode(
+        output_ids,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=True,
     )
-    return tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 
 
 @app.get("/qa/", status_code=200)

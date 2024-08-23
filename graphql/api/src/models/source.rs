4e038f4e01ffd82145a524cafbf6157fcb74811a
Hinome <57831472+RealHinome@users.noreply.github.com>
2024-08-23 17:06:36 +0200

    Add country field

--- graphql/api/src/models/source.rs
+++ graphql/api/src/models/source.rs
@@ -1,12 +1,18 @@
 use crate::models::image::Image;
 use juniper::GraphQLObject;
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 /// A structure representing where the article has been taken.
 #[derive(Clone, Debug, Default, GraphQLObject, Serialize, Deserialize)]
 pub struct Media {
-    pub name: String,
-    pub url: String,
+    /// The country where the media is based.
+    pub country: String,
+    /// Website homepage of media.
     pub media_url: String,
+    /// The media favicon.
     pub media_image: Image,
+    /// The name of the media.
+    pub name: String,
+    /// URL of the article.
+    pub url: String,
 }
